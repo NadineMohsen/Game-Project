df9f3bd344371f70d3b8a3b83e3c5ecf96ce18c9
GrantRT <granto_92@hotmail.com>
2022-01-08 14:06:08 +0000

    added js

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,8 @@
-var javascript;
+var word = '';
+var answers = ['javascript', 'this', 'css', 'html'];
+var button = document.querySelector('#button1');
+
+button.addEventListener('click', function startGame() {
+  var choice = math.floor(math.random() * answers.length);
+  console.log(answers[choice]);
+});
