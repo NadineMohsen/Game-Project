2625b39241f640ef88f0b1b3b8675097154bab80
GrantRT <granto_92@hotmail.com>
2022-01-08 14:11:00 +0000

    fixed "Math"

--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,6 @@ var answers = ['javascript', 'this', 'css', 'html'];
 var button = document.querySelector('#button1');
 
 button.addEventListener('click', function startGame() {
-  var choice = math.floor(math.random() * answers.length);
+  var choice = Math.floor(Math.random() * answers.length);
   console.log(answers[choice]);
 });
